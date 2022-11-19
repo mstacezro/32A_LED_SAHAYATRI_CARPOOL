eff38d756863c3dc16ccd69cf843d4244beb9de1
rushmitkarki <rushmit.karki10@gmail.com>
2022-11-19 18:23:53 +0545

    Added database to viewProfile

--- src/controller/UserController.java
+++ src/controller/UserController.java
@@ -64,4 +64,40 @@ public class UserController {
         int result = dbConnection.manipulate(changeStatus);
         return result;
     }
+    
+    public ResultSet selectdetails(User user){
+        String selectdetails = "select * from user where status='"+"active"+"'";
+        dbConnection = new DbConnection();
+        ResultSet result = dbConnection.retrieve(selectdetails);
+        return result;
+    }
+
+    public int editdetails(User user){
+        String username = user.getUsername();
+        String pass = user.getuserpass();
+        // String repass = user.getuserrepass();
+        String fname = user.getuserfname();
+        String mname = user.getusermname();
+        String lname = user.getuserlname();
+        String gender = user.getusergender();
+        String dob = user.getuserdob();
+        String nationality = user.getusernationality();
+        String address = user.getuseraddress();
+        String email = user.getuseremail();
+        String phone = user.getuserphone();
+        String sq = user.getusersq();
+        String sqAnswer = user.getusersqanswer();
+        String citizen = user.getusercitizen();
+        String licencse= user.getuserlicense();
+        String expiry = user.getuserexpiry();
+
+        String editquery = "update user set(username,password,first_name,middle_name,last_name,gender,dob,nationality,email,phone,sq,sq_ans,citizen,user_Address,license,expiry)"
+        + "values('" + username + "','" + pass + "','" + fname + "','" + mname + "','" + lname + "','" + gender
+        + "','" + dob + "','" + nationality + "','" + email + "','" + phone + "','" + sq
+        + "','"+sqAnswer+"','"+citizen+"','"+address+"','"+licencse+"','"+expiry+"') where status='"+"active"+"'";
+
+        dbConnection = new DbConnection();
+        int result = dbConnection.manipulate(editquery);
+        return result;
+    }
 }
